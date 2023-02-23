cd4db898ecf52dd2cf23062b890c2e9f08857c76
YJL3888 <52924882+YJL3888@users.noreply.github.com>
2023-02-23 13:19:15 -0500

    basic reservation form

--- Reservation/resources/views/livewire/reservation.blade.php
+++ Reservation/resources/views/livewire/reservation.blade.php
@@ -1,87 +1,21 @@
-
-    <!--  Application Details Start -->
-    <div class="w-full bg-grey-500">
-        <div class="container mx-auto py-8">
-            <div class="w-96 mx-auto bg-white rounded shadow">
-
-                <div class="mx-16 py-4 px-8 text-black text-xl font-bold border-b border-grey-500">Reservation
-                </div>
-
-                <form wire:submit.prevent="submitReservation">
-                    <div class="py-4 px-8">
-
-                        <div class="mb-4">
-                            <label class="block text-grey-darker text-sm font-bold mb-2">Name:</label>
-                            <input wire:model="name" class=" border rounded w-full py-2 px-3 text-grey-darker" 
-                            type="text" name="name" placeholder="Enter Your Name">
-                     
-                        </div>
-
-
-                        <div class="mb-4">
-                            <label class="block text-grey-darker text-sm font-bold mb-2">Number of Guests:</label>
-                            <input wire:model="nb_guests" class=" border rounded w-full py-2 px-3 text-grey-darker" 
-                            type="number" name="nb_guests" placeholder="Enter the Number of Guests">
-                      
-                        </div>
-
-
-                        <div class="mb-4">
-                            <label class="block text-grey-darker text-sm font-bold mb-2">Date:</label>
-                            <input wire:model="date" class=" border rounded w-full py-2 px-3 text-grey-darker" 
-                            type="date" name="date">
-                            <p id=error_intake_year></p>
-                        </div>
-
-                        <div class="mb-4">
-                            <button type="submit"
-                                class="mb-2 mx-16 rounded-full py-1 px-24 bg-gradient-to-r from-green-400 to-blue-500 ">
-                                Reserve
-                            </button>
-                        </div>
-
-                    </div>
-                </form>
-
-            </div>
-
-        </div>
-    </div>
-    <!--  Application Details End -->
-
-
-   
-                    </div>
-                </form>
-
-            </div>
-
-        </div>
-    </div>
-    <!-- Student Visa End -->
-
-
-
-
-
-<!-- <div>
+<div>
     <form wire:submit.prevent="submitReservation">
         <div>
             <label>Name:</label>
             <input wire:model="name" type="text" name="name" >
-            @error('name') <span>{{$message}}</span> @enderror
+            @error('name') <span style="color:#FF0000">{{$message}}</span> @enderror
         </div>
 
         <div>
             <label>Number of Guests:</label>
             <input wire:model="nb_guests" type="number" name="nb_guests">
-            @error('nb_guests') <span>{{$message}}</span> @enderror
+            @error('nb_guests') <span style="color:#FF0000">{{$message}}</span> @enderror
         </div>
 
         <div>
             <label>Date:</label>
             <input wire:model="date" type="date" name="date">
-            @error('date') <span>{{$message}}</span> @enderror
+            @error('date') <span style="color:#FF0000">{{$message}}</span> @enderror
         </div>
 
         <div>
@@ -90,4 +24,4 @@
 
     </form>
 </div>
- -->
+ 
--- Reservation/resources/views/reservationPage.blade.php
+++ Reservation/resources/views/reservationPage.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 
 @section('headContent')
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
 @endsection
 
 @section('content')
 
 @livewire('reservation')
+
 
 @endsection
